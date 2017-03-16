7ffed5e239b1730abfc041b7722aaf2c2712b26e
akumar36 <akumar36@192.168.0.11>
2017-03-15 20:54:56 -0700

    more java 8 streams and lamdas

--- src/inst/an/java8/Employee.java
+++ src/inst/an/java8/Employee.java
@@ -1,4 +1,31 @@
 package inst.an.java8;
 
 public class Employee {
+	private Long id;
+	private String name;
+	private Long deptId;
+	private Double salary;
+	public Employee(Long id, String name, Long deptId, Double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.deptId = deptId;
+		this.salary = salary;
+	}
+	public Long getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public Long getDeptId() {
+		return deptId;
+	}
+	public Double getSalary() {
+		return salary;
+	}
+	@Override
+	public String toString() {
+		return "[id=" + id + ", name=" + name + ", deptId=" + deptId + ", salary=" + salary + "]";
+	}
 }
---
+++ src/inst/an/java8/EmployeeApp.java
@@ -0,0 +1,47 @@
+package inst.an.java8;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BinaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class EmployeeApp {
+
+	public static void main(String[] args) {
+		
+		List<Employee> eList = Stream.of(new Employee(1L, "emp1", 3L, 9000D), 
+				new Employee(2L, "emp2", 4L, 7000D), 
+				new Employee(3L, "emp3", 4L, 5000D), 
+				new Employee(4L, "emp4", 2L, 9900D), 
+				new Employee(5L, "emp5", 1L, 5000D), 
+				new Employee(6L, "emp6", 3L, 8000D), 
+				new Employee(7L, "emp7", 1L, 1000D)).collect(Collectors.toList());
+		
+		Map<Long, Employee> deptMaxSalary = new HashMap<>();
+		
+		for(Employee e : eList){
+			if(deptMaxSalary.get(e.getDeptId()) == null){
+				deptMaxSalary.put(e.getDeptId(), e);
+			} else {
+				if(e.getSalary() > deptMaxSalary.get(e.getDeptId()).getSalary()){
+					deptMaxSalary.put(e.getDeptId(), e);
+				}
+			}
+		}
+		
+		for (Map.Entry<Long, Employee> entry : deptMaxSalary.entrySet()) {
+			System.out.println(entry.getKey() + " - " + entry.getValue());
+		}
+		
+		Comparator<Employee> bySalary = Comparator.comparing(Employee::getSalary);
+		
+		eList.stream().collect(
+				Collectors.groupingBy(Employee::getDeptId, 
+						Collectors.reducing(BinaryOperator.maxBy(bySalary)))).forEach((k, v)-> {
+							System.out.println(k +" - "+v);
+						});
+	}
+}
